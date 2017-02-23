e8c8c604afc032a69aa4ee8abd51910a9bc7a27c
Gery Egan <gery.egan@gmail.com>
2017-02-22 18:25:54 -0800

    complete/incomplete logic added

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
 
 class Task extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+         status: this.props.status   
+        };
+    }
     componentDidMount() {
         console.log('RENDERING TASK', this.props.text);
+        console.log('STATE', this.state);
+    }
+    completeTask() {
+        this.setState({
+            status: !this.state.status
+        });
+        console.log('STATE FROM COMPLETETASK CALL', this.state);
+    }
+    completeOrNah() {
+        if (this.state.status) {
+            return 'COMPLETE';
+        } 
+            return 'INCOMPLETE';
+    }
+    buttonTextConditional() {
+        if (this.state.status) {
+            return 'incomplete';
+        } 
+            return 'complete';
     }
     render() {
         return (<View>
             <Text> {this.props.text} </Text>
+            <Text> COMPLETED: {this.completeOrNah()} </Text>
+            <TouchableHighlight onPress={this.completeTask.bind(this)}>
+                <Text> Tap to {this.buttonTextConditional()} </Text>
+            </TouchableHighlight>
         </View>);
     }
 }
