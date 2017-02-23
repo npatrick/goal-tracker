8742aef0c2e934d27beff39720251512e2c166bf
Gery Egan <gery.egan@gmail.com>
2017-02-22 17:43:48 -0800

    Basic add task functionality added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
+import Task from './components/Task';
+import { Input } from './components/common';
 import styles from './styles/styles';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      taskToAdd: '',
+      tasks: []
+     };
+  }
+
+addTask() {
+  const tasks = this.state.tasks.slice();
+  console.log('call of addtask', tasks);
+  tasks.push(this.state.taskToAdd);
+  this.setState({
+    tasks,
+    taskToAdd: ''
+  });
+}
+
+taskTextChange(taskToAdd) {
+  this.setState({ taskToAdd });
+}
+
+renderTasks() {
+  return this.state.tasks.map((task, index) => (<Task key={index} text={task} />)); 
+}
 
     render() {
         return (
@@ -10,10 +37,16 @@ class App extends Component {
         <Text style={styles.welcome}>
           Welcome to GoalTracker!!!
         </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        {this.renderTasks()}
+        <Input 
+          placeholder='Add a Task!' 
+          onChangeText={this.taskTextChange.bind(this)} 
+          value={this.state.taskToAdd} 
+        />
+        <TouchableHighlight onPress={this.addTask.bind(this)}>
+          <Text> Add </Text>
+        </TouchableHighlight>
+
       </View>
         );
     }
