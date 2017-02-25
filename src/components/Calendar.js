9ef5c5f49b723c3db822752f3d8383dd9051e9fd
Gery Egan <gery.egan@gmail.com>
2017-02-24 16:26:22 -0800

    test calendar component created

---
+++ src/components/Calendar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Calendar extends Component {
+    state = { view: 'month' }
+    render() {
+        return (
+            <View>
+                <Text>CALENDAR</Text>
+                <Text>CALENDAR</Text>
+                <Text>CALENDAR</Text>
+                <Text>CALENDAR</Text>
+                <Text>CALENDAR</Text>
+                <Text>CALENDAR</Text>
+            </View>
+        );
+    }
+}
+
+export default Calendar;
