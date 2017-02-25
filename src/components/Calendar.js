e85267930fbbd3a44ab6d8a6a038891741a438e6
Gery Egan <gery.egan@gmail.com>
2017-02-24 18:29:54 -0800

    styling text and bgcolor

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import styles from '../styles/styles';
+
 
 class Calendar extends Component {
     state = { view: 'month' }
     render() {
         return (
-            <View>
-                <Text>CALENDAR</Text>
-                <Text>CALENDAR</Text>
-                <Text>CALENDAR</Text>
-                <Text>CALENDAR</Text>
-                <Text>CALENDAR</Text>
-                <Text>CALENDAR</Text>
+            <View style={styles.container}>
+                <Text style={styles.welcome}>CALENDAR</Text>
+                <Text style={styles.welcome}>CALENDAR</Text>
+                <Text style={styles.welcome}>CALENDAR</Text>
+                <Text style={styles.welcome}>CALENDAR</Text>
             </View>
         );
     }
